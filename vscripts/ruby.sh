e2b746e8c157c3cbde8ca0e8defec6ddfd7aed96
grayoverride <johan.b.damm@gmail.com>
2013-11-25 15:19:01 +0100

    Separeted passenger and ruby, also added mongo install

---
+++ vscripts/ruby.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#rvm
+apt-get install -y curl
+\curl -L https://get.rvm.io | bash
+source /etc/profile
+#uncomment to install a specific rubyversion a launch, 1.9.3-p125 is an example
+#rvm install ruby-1.9.3-p125
