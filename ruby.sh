30d10151ba5ec7cc14de802575346f48ea2c26ea
grayoverride <johan.b.damm@gmail.com>
2013-11-25 14:16:54 +0100

    spelling

--- ruby.sh
+++ ruby.sh
@@ -21,4 +21,4 @@ sudo apt-get remove -y expect
 sudo sed -i '$a LoadModule passenger_module /usr/local/rvm/gems/ruby-1.9.3-p125@global/gems/passenger-4.0.25/buildout/apache2/mod_passenger.so' /etc/apache2/apache2.conf
 sudo sed -i '$a 	PassengerRoot /usr/local/rvm/gems/ruby-1.9.3-p125@global/gems/passenger-4.0.25' /etc/apache2/apache2.conf
 sudo sed -i '$a 	PassengerDefaultRuby /usr/local/rvm/wrappers/ruby-1.9.3-p125/ruby' /etc/apache2/apache2.conf
-echo"to install a specifict rubyversion run 'rvm install ruby-version' or for the latest and greatest 'rvm install ruby'"
+echo"to install a specific rubyversion run 'rvm install ruby-version' or for the latest and greatest 'rvm install ruby'"
