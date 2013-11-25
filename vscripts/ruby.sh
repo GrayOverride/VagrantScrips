63ade6d40501b459293ed3e9947203465702472b
grayoverride <johan.b.damm@gmail.com>
2013-11-25 16:11:38 +0100

    Minor fixes

--- vscripts/ruby.sh
+++ vscripts/ruby.sh
@@ -3,5 +3,5 @@
 apt-get install -y curl
 \curl -L https://get.rvm.io | bash
 source /etc/profile
-#uncomment to install a specific rubyversion a launch, 1.9.3-p125 is an example
-#rvm install ruby-1.9.3-p125
+#comment to install a specific rubyversion a launch, 1.9.3-p125 is an example
+rvm install ruby-1.9.3-p125
