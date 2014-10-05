c64dadeccd54e937a647291451a3d3c3dc25b45e
grayoverride <johan.b.damm@gmail.com>
2014-10-06 01:21:38 +0200

    added laravel

---
+++ vscripts/laravel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+##install dependencys
+sudo apt-get install -y php5-cli apache2 php5-mcrypt
+curl -sS https://getcomposer.org/installer | php 
+sudo mv composer.phar /usr/local/bin/composer
+cd /vagrant/ && https://github.com/GrayOverride/VagrantScrips.git
+sudo php5enmod mcrypt
+cd mkweb && composer install
+
