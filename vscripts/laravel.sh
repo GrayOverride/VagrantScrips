169d838a9d324dac5c4d2f5820d98337d9bc9d5c
johan damm <johan.b.damm@gmail.com>
2014-10-06 01:29:49 +0200

    Update laravel.sh

--- vscripts/laravel.sh
+++ vscripts/laravel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ##install dependencys
-sudo apt-get install -y php5-cli apache2 php5-mcrypt curl
+sudo apt-get install -y php5-cli apache2 php5-mcrypt curl git
 curl -sS https://getcomposer.org/installer | php 
 sudo mv composer.phar /usr/local/bin/composer
-cd /vagrant/ && https://github.com/GrayOverride/VagrantScrips.git
+cd /vagrant/ && git clone https://github.com/PhantomOverride/mkweb.git
 sudo php5enmod mcrypt
 cd mkweb && composer install
 
