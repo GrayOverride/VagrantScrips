0a498898a97f42617e263ea43ce14bb8c0094a87
johan damm <johan.b.damm@gmail.com>
2014-10-06 02:04:04 +0200

    Update laravel.sh

--- vscripts/laravel.sh
+++ vscripts/laravel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ##install dependencys
+sudo apt-get update -y
 sudo apt-get install -y php5-cli apache2 php5-mcrypt curl git
 curl -sS https://getcomposer.org/installer | php 
 sudo mv composer.phar /usr/local/bin/composer
